04c67292bef556af79b356da204f7604ae045715
Arjun Guha <a.guha@northeastern.edu>
2026-05-03 12:46:01 -0400

    Propagate Arrow stublibs rpath

--- scripts/gen-rpath-sexp.sh
+++ scripts/gen-rpath-sexp.sh
@@ -10,11 +10,12 @@ Darwin*) flags='"-Wl,-rpath,@loader_path"' ;;
 esac
 
 if test -n "${OPAM_SWITCH_PREFIX:-}"; then
-  flags="$flags \"-L$OPAM_SWITCH_PREFIX/lib/stublibs\""
+  stublibs="$OPAM_SWITCH_PREFIX/lib/stublibs"
+  flags="$flags \"-L$stublibs\" \"-Wl,-rpath,$stublibs\""
 elif command -v opam >/dev/null 2>&1; then
   stublibs="$(opam var stublibs 2>/dev/null || true)"
   if test -n "$stublibs"; then
-    flags="$flags \"-L$stublibs\""
+    flags="$flags \"-L$stublibs\" \"-Wl,-rpath,$stublibs\""
   fi
 fi
 
